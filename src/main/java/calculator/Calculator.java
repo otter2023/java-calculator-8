9aa9b83535775bec09dbf02e79b0809df20459dd
Runea <otter2023@naver.com>
2025-10-16 23:31:22 +0900

    feat: 문자열을 구분자 기준으로 슬라이싱 후 계산 결과 출력

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,38 +1,48 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
     String expression;
-    List<Character> separators;
+    Character separators;
     int result;
 
     public Calculator(String expression) {
         this.expression = expression;
-        separators = new ArrayList<>();
-        separators.add(';');
-        separators.add('.');
+        result = 0;
+        separators = ',';
     }
 
     public void calculate() {
 
+        String[] number;
+
         if (isCustomExpression()){
-            separators.clear();
-            separators.add(getCustomExpression());
+            separators = getCustomExpression();
+            expression = expression.substring(5);
         }
+
+        expression = expression.replace(separators, ',');
+        expression = expression.replace(';', ',');
+
+        number = expression.split(",");
+
+        System.out.println(Arrays.toString(number));
 
         System.out.println(separators);
 
-        result = 30;
+        for(String a : number){
+            result += Integer.parseInt(a);
+        }
     }
 
     public boolean isCustomExpression(){
-        return !expression.startsWith("/^[0-9]*$");
+        return !expression.matches("^[0-9].*");
     }
 
     public char getCustomExpression(){
-
         if (expression.charAt(0) != '/' || expression.charAt(1) != '/' || expression.charAt(3) != '\\' || expression.charAt(4) != 'n'){
             throw new IllegalArgumentException("올바른 커스텀 문자열 형식이 아닙니다.");
         }
