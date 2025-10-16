8c3c410f145b28e5bcd0f3cf9a1bf93e445c217c
Runea <otter2023@naver.com>
2025-10-16 21:51:20 +0900

    feat: 콘솔 입출력 구현

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class CalculatorController {
+
+    private final CalculatorView calculatorView;
+
+    public CalculatorController(CalculatorView calculatorView) {
+        this.calculatorView = calculatorView;
+    }
+
+    public void run() {
+        String expression = calculatorView.input();
+        Calculator calculator = new Calculator(expression);
+        calculatorView.output(calculator.result);
+    }
+}
