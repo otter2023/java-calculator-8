00567fb5f2436cb18b1ee76b4dd78eeaaa62fd1f
Runea <otter2023@naver.com>
2025-10-16 23:42:52 +0900

    feat: 빈 문자열 입력 시 0 반환 (공백 X)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -19,6 +19,10 @@ public class Calculator {
 
         String[] number;
 
+        if (expression.isEmpty()){
+            return;
+        }
+
         if (isCustomExpression()){
             separators = getCustomExpression();
             expression = expression.substring(5);
