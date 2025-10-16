be386874a1a8a914d82e77d1503e13362eab72f0
Runea <otter2023@naver.com>
2025-10-16 21:50:43 +0900

    feat: Calculator 객체 생성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Calculator {
+    String expression;
+    String[] separators;
+    int result;
+
+    public Calculator(String expression) {
+        this.expression = expression;
+    }
+
+    public int calculate(String expression) {
+        return 0;
+    }
+}
