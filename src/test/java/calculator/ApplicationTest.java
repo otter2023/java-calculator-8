cf0497d7fbba2071be1d86379d32ce75ffe28bcc
Runea <otter2023@naver.com>
2025-10-19 22:32:51 +0900

    test: 기본 구분자 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -11,8 +11,16 @@ class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n1;4");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
         });
     }
 
