170861d63b2ee6f6e497f4f05a855da30010de53
Runea <otter2023@naver.com>
2025-10-19 21:02:13 +0900

    test: 예외 처리 로직 테스트 케이스 작성

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,10 +17,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 구분자_외의_문자테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
+            assertThatThrownBy(() -> runException("1,-2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구분자와 숫자 이외의 문자가 있습니다.")
+        );
+    }
+
+    @Test
+    void 문자열의_시작과_끝_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException(",1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("문자열은 숫자로 시작하고 끝나야 합니다.")
+        );
+    }
+
+    @Test
+    void 커스텀_문자열_지정_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("1*2*3//*\n"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 커스텀 문자열 형식이 아닙니다.")
+        );
+    }
+
+    @Test
+    void 연속된_구분자_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("1,,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구분자는 연속해서 등장할 수 없습니다.")
         );
     }
 
